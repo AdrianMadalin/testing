71d44e21944b30e81ccf40d023f72eae423b3ac4
AdrianMadalin <adrian.madalin.dinu@gmail.com>
2018-04-27 17:58:58 +0300

    Update gallery.component.ts

--- client/src/app/gallery/gallery.component.ts
+++ client/src/app/gallery/gallery.component.ts
@@ -1,5 +1,7 @@
 import {Component, HostListener, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {HttpClient, HttpEventType} from "@angular/common/http";
+import {Router, RouterLinkActive} from "@angular/router";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-gallery',
@@ -8,12 +10,12 @@ import {HttpClient, HttpEventType} from "@angular/common/http";
 })
 export class GalleryComponent implements OnInit, OnChanges {
 
-  selectedFile: File = null;
   selectedMovieFile: File = null;
   video = document.querySelector('#vid');
   clicked: Boolean = true;
   isFullSize: Boolean = true;
-  counter: Number = 0;
+  imageFormData = new FormData();
+  uploadedImages: any;
 
   // @HostListener("window:scroll", []) private onScroll($event:Event):void {
   //   console.log(event);
@@ -21,10 +23,16 @@ export class GalleryComponent implements OnInit, OnChanges {
   //   console.log(window.scrollY);
   // };
 
-  constructor(private _httpClient: HttpClient) {
+  constructor(private _httpClient: HttpClient,
+              private _router: Router,
+              private _authService: AuthService) {
   }
 
   ngOnInit() {
+    this._authService.getUploadedImages().subscribe((res) => {
+      this.uploadedImages = res['images'];
+      console.log(this.uploadedImages);
+    });
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -36,22 +44,17 @@ export class GalleryComponent implements OnInit, OnChanges {
   }
 
   onFileSelected(event) {
-    console.log(event.target.files);
-    this.selectedFile = <File>event.target.files;
-    this.counter = event.target.files.length;
-    console.log(this.selectedFile);
-
-  }
-
-  onUpload() {
-    const url = '/api-images/kungfu';
-    const fd = new FormData();
-    for (let i = 0; i < this.counter; i++) {
-      if (this.selectedFile[i].type === 'image/jpeg') {
-        fd.append('image', this.selectedFile[i], this.selectedFile[i].name);
+    for (let i = 0; i < event.target.files.length; i++) {
+      if (event.target.files[i].type === 'image/jpeg') {
+        this.imageFormData.append('image', event.target.files[i], event.target.files[i].name);
       }
     }
-    this._httpClient.post(url, fd, {
+  }
+
+  onFileUpload() {
+    const url = '/api-images/kungfu';
+
+    this._httpClient.post(url, this.imageFormData, {
       reportProgress: true,
       observe: 'events'
     }).subscribe(
@@ -59,12 +62,26 @@ export class GalleryComponent implements OnInit, OnChanges {
         if (event.type === HttpEventType.UploadProgress) {
           console.log(`Upload progress ${Math.round(event.loaded / event.total * 100)}%`);
         } else if (event.type === HttpEventType.Response) {
+          this._router.navigate(['/galerie']);
           console.log(event);
+          this._authService.getUploadedImages().subscribe((res) => {
+            this.uploadedImages = res['images'];
+          });
         } else {
           // console.log(event);
         }
       }
     );
+  }
+
+  onDeleteImage(event) {
+    const id = {id: event.target.value};
+    this._authService.removeUploadedImage(id).subscribe((res) => {
+      console.log(res);
+      this._authService.getUploadedImages().subscribe((res) => {
+        this.uploadedImages = res['images'];
+      });
+    })
   }
 
   onMovieSelected(event) {
@@ -87,6 +104,7 @@ export class GalleryComponent implements OnInit, OnChanges {
         if (event.type === HttpEventType.UploadProgress) {
           console.log(`Upload progress ${Math.round(event.loaded / event.total * 100)}%`);
         } else if (event.type === HttpEventType.Response) {
+
           console.log(event);
         } else {
           // console.log(event);
@@ -94,10 +112,9 @@ export class GalleryComponent implements OnInit, OnChanges {
       });
   }
 
-
   onClick(event) {
     event.controls = false;
-    console.log(event)
+    console.log(event);
     if (this.clicked) {
       event.target.play();
       this.clicked = !this.clicked
